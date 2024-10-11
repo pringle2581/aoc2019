98f3e5c438b12d864149013b2e00e53b521c8a9e
pringle2581 <kmjohnstonjr@gmail.com>
2024-10-10 20:33:20 -0400

    day 3

--- Program.cs
+++ Program.cs
@@ -1,14 +1,20 @@
 ﻿using aoc2019.day1;
 using aoc2019.day2;
+using aoc2019.day3;
 
 string input = "../../../input/";
 
-int[] day1 = Day1.Solve(File.ReadAllLines(input+1));
+int[] day1 = Day1.Solve(File.ReadAllLines(input + 1));
 Console.WriteLine($"Day 1 Part 1: {day1[0]}");
 Console.WriteLine($"Day 1 Part 2: {day1[1]}");
 Console.WriteLine("");
 
-int[] day2 = Day2.Solve(File.ReadAllLines(input+2));
+int[] day2 = Day2.Solve(File.ReadAllLines(input + 2));
 Console.WriteLine($"Day 2 Part 1: {day2[0]}");
 Console.WriteLine($"Day 2 Part 2: {day2[1]}");
+Console.WriteLine("");
+
+int[] day3 = Day3.Solve(File.ReadAllLines(input + 3));
+Console.WriteLine($"Day 3 Part 1: {day3[0]}");
+Console.WriteLine($"Day 3 Part 2: {day3[1]}");
 Console.WriteLine("");
---
+++ src/day3/3.cs
@@ -0,0 +1,52 @@
+﻿using System.Text.RegularExpressions;
+namespace aoc2019.day3
+{
+    public class Day3
+    {
+        public static int[] Solve(string[] input)
+        {
+            List<int> part1 = [], part2 = [];
+            var wire1 = FindPath(input[0].Split(","));
+            var wire2 = FindPath(input[1].Split(","));
+            var intersections = wire1.Intersect(wire2);
+            foreach (var result in intersections)
+            {
+                part1.Add(result.Item1 + result.Item2);
+                part2.Add(wire1.IndexOf(result) + wire2.IndexOf(result) + 2);
+            }
+            return [part1.Min(), part2.Min()];
+        }
+        static List<(int, int)> FindPath(string[] instructions)
+        {
+            var path = new List<(int, int)> ();
+            int x = 0, y = 0;
+            foreach (string instr in instructions)
+            {
+                Regex regex = new(@"([A-Z])(\d+)");
+                Match result = regex.Match(instr);
+                string dir = result.Groups[1].Value;
+                int steps = int.Parse(result.Groups[2].Value);
+                foreach (int step in Enumerable.Range(1, steps))
+                {
+                    switch (dir)
+                    {
+                        case "U":
+                            y += 1;
+                            break;
+                        case "D":
+                            y -= 1;
+                            break;
+                        case "L":
+                            x -= 1;
+                            break;
+                        case "R":
+                            x += 1;
+                            break;
+                    }
+                    path.Add((x, y));
+                }
+            }
+            return path;
+        }
+    }
+}
