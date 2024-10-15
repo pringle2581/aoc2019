06f86feea26cc060c3a7e007934cf0eb90589407
pringle2581 <kmjohnstonjr@gmail.com>
2024-10-15 11:41:01 -0400

    day 6

--- Program.cs
+++ Program.cs
@@ -3,30 +3,19 @@ using aoc2019.day2;
 using aoc2019.day3;
 using aoc2019.day4;
 using aoc2019.day5;
+using aoc2019.day6;
 
 string input = "../../../input/";
 
-int[] day1 = Day1.Solve(File.ReadAllLines(input + 1));
-Console.WriteLine($"Day 1 Part 1: {day1[0]}");
-Console.WriteLine($"Day 1 Part 2: {day1[1]}");
-Console.WriteLine("");
+void PrintResults(int day, int[] results)
+{
+    Console.WriteLine($"Day {day} Part 1: {results[0]}");
+    Console.WriteLine($"Day {day} Part 2: {results[1]}\n");
+}
 
-int[] day2 = Day2.Solve(File.ReadAllLines(input + 2));
-Console.WriteLine($"Day 2 Part 1: {day2[0]}");
-Console.WriteLine($"Day 2 Part 2: {day2[1]}");
-Console.WriteLine("");
-
-int[] day3 = Day3.Solve(File.ReadAllLines(input + 3));
-Console.WriteLine($"Day 3 Part 1: {day3[0]}");
-Console.WriteLine($"Day 3 Part 2: {day3[1]}");
-Console.WriteLine("");
-
-int[] day4 = Day4.Solve(File.ReadAllLines(input + 4));
-Console.WriteLine($"Day 4 Part 1: {day4[0]}");
-Console.WriteLine($"Day 4 Part 2: {day4[1]}");
-Console.WriteLine("");
-
-int[] day5 = Day5.Solve(File.ReadAllLines(input + 5));
-Console.WriteLine($"Day 5 Part 1: {day5[0]}");
-Console.WriteLine($"Day 5 Part 2: {day5[1]}");
-Console.WriteLine("");
+PrintResults(1, Day1.Solve(File.ReadAllLines(input + 1)));
+PrintResults(2, Day2.Solve(File.ReadAllLines(input + 2)));
+PrintResults(3, Day3.Solve(File.ReadAllLines(input + 3)));
+PrintResults(4, Day4.Solve(File.ReadAllLines(input + 4)));
+PrintResults(5, Day5.Solve(File.ReadAllLines(input + 5)));
+PrintResults(6, Day6.Solve(File.ReadAllLines(input + 6)));
---
+++ src/day6/6.cs
@@ -0,0 +1,45 @@
+﻿namespace aoc2019.day6
+{
+    public class Day6
+    {
+        static public int[] Solve(string[] input)
+        {
+            Dictionary<string, string> orbits = [];
+            foreach (string line in input)
+            {
+                string[] split = line.Split(")");
+                orbits.Add(split[1], split[0]);
+            }
+
+            int part1 = 0, part2 = 0;
+            foreach (var orbit in orbits)
+            {
+                part1 += CountOrbits(orbit.Value, 0);
+            }
+            List<string> youparents = [], sanparents = [];
+            GetParents("YOU", youparents);
+            GetParents("SAN", sanparents);
+            part2 += youparents.Except(sanparents).Count();
+            part2 += sanparents.Except(youparents).Count();
+            return [part1, part2];
+
+            int CountOrbits(string node, int sum)
+            {
+                if (node != "COM")
+                {
+                    sum = CountOrbits(orbits[node], sum);
+                }
+                return sum + 1;
+            }
+
+            void GetParents(string node, List<string> parents)
+            {
+                if (node != "COM")
+                {
+                    parents.Add(orbits[node]);
+                    GetParents(orbits[node], parents);
+                }
+            }
+        }
+    }
+}
