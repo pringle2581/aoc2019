ace7f6b8159b16a0cdd9cb50b5105cff1d4b6870
pringle2581 <kmjohnstonjr@gmail.com>
2024-10-30 12:23:52 -0400

    day 16 part 2

--- src/day16/16.cs
+++ src/day16/16.cs
@@ -6,7 +6,29 @@ namespace aoc2019.day16
         {
             int[] insignal = ParseSignal(input[0]);
             int[] part1 = FFT(insignal, 100)[0..8];
-            return [string.Join("", part1), ""];
+            int offset = int.Parse(input[0][0..7]);
+            int[] pt2signal = new int[insignal.Length * 10000];
+            for (int i = 0; i < insignal.Length * 10000; i++)
+            {
+                pt2signal[i] = insignal[i % insignal.Length];
+            }
+            int[] part2 = FFT2(pt2signal, 100, offset)[offset..(offset + 8)];
+            return [string.Join("", part1), string.Join("", part2)];
+        }
+
+        static int[] FFT2(int[] insignal, int phases, int offset)
+        {
+            int[] outsignal = [.. insignal];
+            for (int phase = 0; phase < phases; phase++)
+            {
+                int sum = 0;
+                for (int i = outsignal.Length - 1; i >= offset; i--)
+                {
+                    sum += outsignal[i];
+                    outsignal[i] = sum % 10;
+                }
+            }
+            return outsignal;
         }
 
         static int[] FFT(int[] insignal, int phases)
